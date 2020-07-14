589379f9805c89f0e2af269f86b28f422c85a2b2
John <John@HinckleyResearch.com>
2020-07-14 12:27:04 -0400

    Added code to set the TextView text color to a fixed value.

--- ColorControl/ColorControl.Droid/Views/FirstView.cs
+++ ColorControl/ColorControl.Droid/Views/FirstView.cs
@@ -1,11 +1,17 @@
 using Android.App;
 using Android.OS;
 
+// 1.
+using Android.Widget;   // Need this for declaring TextView widget reference
+
 namespace ColorControl.Droid.Views
 {
     [Activity(Label = "View for FirstViewModel")]
     public class FirstView : BaseView
     {
+        // 2.
+        private TextView _tv;   // TextView widget reference
+
         protected override int LayoutResource => Resource.Layout.FirstView;
 
         protected override void OnCreate(Bundle bundle)
@@ -13,6 +19,12 @@ namespace ColorControl.Droid.Views
             base.OnCreate(bundle);
 
             SupportActionBar.SetDisplayHomeAsUpEnabled(false);
+
+            // 3.
+            _tv = FindViewById<TextView>(Resource.Id.textView1);    // reference to the TextView widget
+
+            // 4.
+            _tv.SetTextColor(Android.Graphics.Color.Red);   // Set the text color to red
         }
     }
 }
